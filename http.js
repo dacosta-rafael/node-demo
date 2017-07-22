28f55d2dcf9cabaf96edc168bab10705249b5243
Rafael da Costa <dacosta_rafael@hotmail.com>
2017-07-22 08:47:28 -0400

    update code for rest

--- http.js
+++ http.js
@@ -14,10 +14,22 @@ http.createServer( function(request, response) {
 		{ id: 2, title:'book2' },
 		{ id: 3, title:'book3' }
 	];
+
+
+    var cake_options = 
+        { 
+          flavors: [vanilla, chocolate],
+          icing: [butter cream, whip cream]
+        };
+
 	
 	var bookid = 1; 
 	bookid = queryData.bookid;
+///
 
+if (queryData.cakeOptions){
+   response.write( JSON.stringify(cake_options) ); 
+}
 
 	switch (queryData.q) {
 			case 'Ping':
@@ -99,6 +111,7 @@ break;
                 case 'Email Address':
                         response.write('dacosta_rafael@hotmail.com');
                         break;
+
 
 		default:
 			response.write('Not applicable');
